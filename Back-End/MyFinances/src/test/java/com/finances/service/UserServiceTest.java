ecb1768cb4c7081157440e5f0ae6f5b03ac0b388
Paulounk <paulo.job@hotmail.com>
2021-10-10 20:18:08 -0300

    User Service - Integrations tests with JUNIT and Mockito

--- Back-End/MyFinances/src/test/java/com/finances/service/UserServiceTest.java
+++ Back-End/MyFinances/src/test/java/com/finances/service/UserServiceTest.java
@@ -1,17 +1,26 @@
 package com.finances.service;
 
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.finances.exception.AuthenticationException;
 import com.finances.exception.BusinessRuleException;
 import com.finances.model.User;
 import com.finances.repository.UserRepository;
+import com.finances.service.impl.UserServiceImpl;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -21,31 +30,94 @@ public class UserServiceTest {
 	@Autowired
 	UserService service;
 	
-	@Autowired
+	//Através da anotation MockBean o mockito cria uma instância fake de uma classe para ser testada
+	@MockBean
 	UserRepository repository;
 	
+	@BeforeEach
+	public void setUp() {
+		service = new UserServiceImpl(repository);
+	}
+	
+	@DisplayName("Não deve retornar exception se a autenticação estiver com dados corretos")
+	@Test
+	public void mustAuthenticateAUserWithSuccess() {
+		
+		//Scenery
+		String email = "paulo@hotmail.com";
+		String password = "123456";
+		
+		User user = User.builder().email(email).password(password).id(1l).build();
+		Mockito.when(repository.findByEmailContainingIgnoreCase(email)).thenReturn(Optional.of(user));
+		
+		//Action
+		User result = service.authenticate(email,password);
+		
+		//Verification
+		Assertions.assertNotNull(result);
+	
+	}
+	
+	@DisplayName("Deve retornar exception quando não for encontrado o e-mail para autenticação")
+	@Test
+	public void shouldReturnExceptionWhenTheEmailForAuthenticationDoesNotExist() {
+		
+		//Scenery
+		Mockito.when(repository.findByEmailContainingIgnoreCase(Mockito.anyString())).thenReturn(Optional.empty());
+		
+		//Action
+		Throwable exception = org.assertj.core.api.Assertions.catchThrowable(() -> service.authenticate("paulo@hotmail.com", "456789"));
+		
+		//Verification
+		org.assertj.core.api.Assertions.assertThat(exception)
+			.isInstanceOf(AuthenticationException.class)
+			.hasMessage("User not found with the email entered!");	
+		
+	}
+	
+	@DisplayName("Deve retornar exception quando senha estiver inválida")
+	@Test
+	public void shouldReturnErrorWhenPasswordIsInvalid() {
+		
+		//Scenery
+		String senha = "123456";
+		User user = User.builder().email("paulo@hotmail.com").password(senha).build();
+		Mockito.when(repository.findByEmailContainingIgnoreCase(Mockito.anyString())).thenReturn(Optional.of(user));
+		
+		//Action
+		Throwable exception = org.assertj.core.api.Assertions.catchThrowable(() -> service.authenticate("paulo@hotmail.com", "456789"));
+		
+		//Verification
+		org.assertj.core.api.Assertions.assertThat(exception)
+		.isInstanceOf(AuthenticationException.class)
+		.hasMessage("Invalid Password!");	
+		
+	}
+	
+	@DisplayName("Não deve retornar exception se for um novo e-mail para cadastrado")
 	@Test
 	public void mustValidateEmail() {
 		
 		Assertions.assertDoesNotThrow(() -> {
+			
 			//Scenery
-			repository.deleteAll();
+			Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 				
 			//Action 
-			service.validateEmail("pedro@hotmail.com");
+			service.validateEmail("paulo@hotmail.com");
 		});
 	}
 	
+	@DisplayName("Deve retornar exception se e-mail já estiver cadastrado")
 	@Test
 	public void shouldShowErrorWhenValidatingEmailWhenItAlreadExists() {
 		
 		Assertions.assertThrows(BusinessRuleException.class, () -> {
 			//Scenery
-			User user = User.builder().name("pedro").email("pedro@hotmail.com").password("123456").build();
-			repository.save(user);
+			Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		
 			//Action 
-			service.validateEmail("pedro@hotmail.com");
+			service.validateEmail("paulo@hotmail.com");
 		});
 	}
 
